7bd328d7c1244a56764b5dffbc8376b30ec00a64
Carl Bjerggaard <carl.bjerggaard@gmail.com>
2013-03-29 18:51:37 +0100

    added meta-data to manifest and all buttons now link to activities

--- exercise02/RobberLanguage/src/com/example/robberlanguage/MainActivity.java
+++ exercise02/RobberLanguage/src/com/example/robberlanguage/MainActivity.java
@@ -1,17 +1,13 @@
 package com.example.robberlanguage;
 
-import com.example.myfirstapp.DisplayMessageActivity;
-import com.example.myfirstapp.R;
-
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -28,33 +24,24 @@ public class MainActivity extends Activity {
 	/** Called when the user clicks the Translate to button */
 	public void translateTo(View view) {
 		Intent intent = new Intent(this, TranslateToActivity.class);
-		EditText editText = (EditText) findViewById(R.id.edit_message);
-		String message = editText.getText().toString();
-		intent.putExtra(EXTRA_MESSAGE, message);
 		startActivity(intent);
 	}
 	
 	/** Called when the user clicks the Translate from button */
 	public void translateFrom(View view) {
 		Intent intent = new Intent(this, TranslateFromActivity.class);
-		EditText editText = (EditText) findViewById(R.id.edit_message);
-		String message = editText.getText().toString();
-		intent.putExtra(EXTRA_MESSAGE, message);
 		startActivity(intent);
 	}
 	
 	/** Called when the user clicks the About button */
 	public void about(View view) {
 		Intent intent = new Intent(this, AboutActivity.class);
-		EditText editText = (EditText) findViewById(R.id.edit_message);
-		String message = editText.getText().toString();
-		intent.putExtra(EXTRA_MESSAGE, message);
 		startActivity(intent);
 	}
 	
 	/** Called when the user clicks the Quit button */
 	public void quit(View view) {
-		//quit activity
+		finish();
 	}
 
 }
--- exercise02/RobberLanguage/src/com/example/robberlanguage/TranslateFromActivity.java
+++ exercise02/RobberLanguage/src/com/example/robberlanguage/TranslateFromActivity.java
@@ -1,22 +1,34 @@
 package com.example.robberlanguage;
 
-import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 
 public class TranslateFromActivity extends Activity {
 
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_translate_from);
+
+		// Make sure we're running on Honeycomb or higher to use ActionBar APIs
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			// Show the Up button in the action bar.
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.translate_from, menu);
-		return true;
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
-
 }
