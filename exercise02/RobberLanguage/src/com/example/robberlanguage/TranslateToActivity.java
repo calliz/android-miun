3ca1dc3f6831e3c0ce9386a40491165ec88da046
Carl Bjerggaard <carl.bjerggaard@gmail.com>
2013-05-26 18:03:27 +0200

    added contextmenu to activity_Translate_to

--- exercise02/RobberLanguage/src/com/example/robberlanguage/TranslateToActivity.java
+++ exercise02/RobberLanguage/src/com/example/robberlanguage/TranslateToActivity.java
@@ -5,9 +5,18 @@ import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.text.Editable;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class TranslateToActivity extends Activity {
+
+	private EditText translatedText;
 
 	@SuppressLint("NewApi")
 	@Override
@@ -20,6 +29,11 @@ public class TranslateToActivity extends Activity {
 			// Show the Up button in the action bar.
 			getActionBar().setDisplayHomeAsUpEnabled(true);
 		}
+
+		// Context menu. Could be changed to Contextual Action Mode for
+		// Android3.0 +
+		translatedText = (EditText) findViewById(R.id.translated_text);
+		registerForContextMenu(translatedText);
 	}
 
 	@Override
@@ -31,5 +45,29 @@ public class TranslateToActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	/** Called when the user clicks the Translate button */
+	public void translateText(View view) {
+		EditText translateText = (EditText) findViewById(R.id.translate_text);
+		if (translateText.getText().length() == 0) {
+			Toast.makeText(this, "There is nothing to translate!",
+					Toast.LENGTH_SHORT).show();
+		} else {
+			Editable input = translateText.getText();
+			translatedText.append("\n" + input);
+			input.clear();
+		}
+
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.context_menu, menu)
+	}
+	
+	
 
 }
