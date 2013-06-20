3f1c4c388f48243bfa7d5da4307c423d659c3c0f
Carl Bjerggaard <carl.bjerggaard@gmail.com>
2013-06-21 01:03:00 +0200

    added exercise03

---
+++ exercise03/MyCalendar/src/com/example/mycalendar/CalendarView.java
@@ -0,0 +1,14 @@
+package com.example.mycalendar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CalendarView extends View {
+
+	public CalendarView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+}
